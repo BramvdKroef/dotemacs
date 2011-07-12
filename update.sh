0412842424e12d5cb9188fd9f77ec8bc35e777f0
Bram van der Kroef <bram@vanderkroef.net>
2011-07-12 13:09:53 -0500

    Added bitlbee update and removed icicles.

--- update.sh
+++ update.sh
@@ -22,18 +22,5 @@ cd $BASEDIR/yasnippet
 svn update
 rake compile
 
-#cd $BASEDIR/icicles
-# http://www.emacswiki.org/emacs/Icicles_-_Libraries
-#wget "http://www.emacswiki.org/emacs/download/icicles.el" 
-#wget "http://www.emacswiki.org/emacs/download/icicles-chg.el" 
-#wget "http://www.emacswiki.org/emacs/download/icicles-cmd1.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-cmd2.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-doc1.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-doc2.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-face.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-fn.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-mac.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-mcmd.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-mode.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-opt.el"
-#wget "http://www.emacswiki.org/emacs/download/icicles-var.el"
+cd $BASEDIR/bitlbee
+git pull
