6c5dc1cf5477e52b27bc897c7bcb11162d595430
Bram van der Kroef <bram@vanderkroef.net>
2012-09-27 07:59:39 -0500

    Added new file for the 'auto-mode-alist list.

---
+++ automode.el
@@ -0,0 +1,18 @@
+;; open .h files in c++ mode instead of c mode
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;; open drupal modules in php mode
+(add-to-list 'auto-mode-alist '("\\.module\\'" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+(add-to-list 'auto-mode-alist '("\\.inc\\'" . php-mode))
+
+;; cakephp views
+(add-to-list 'auto-mode-alist '("\\.ctp\\'" . html-mode))
+
+(add-to-list 'auto-mode-alist '("\\.json$" . javascript-mode))
+(add-to-list 'auto-mode-alist '("\\.conkerorrc\\'" . javascript-mode))
+
+(add-to-list 'auto-mode-alist '("\\.stumpwmrc\\'" . lisp-mode))
+(add-to-list 'auto-mode-alist '("\\.[Cc][Ss][Vv]\\'" . csv-mode))
+(add-to-list 'auto-mode-alist '("\\.doc\\'" . no-word))
+(add-to-list 'auto-mode-alist '("\\.ledger\\'" . ledger-mode))
