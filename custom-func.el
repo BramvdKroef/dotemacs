3ea7370d82f26762207cbecbecc0c60d55fdf1ae
Bram van der Kroef <bram@vanderkroef.net>
2018-09-05 09:42:32 -0500

    Fixing documentation. Removing obsolete funcs and adding funcs.

--- custom-func.el
+++ custom-func.el
@@ -1,12 +1,17 @@
-;; Indent pasted code
+;;; custom-func ---  Custom functions
+;;; Code:
+;;; Commentary:
+
+
 (defadvice yank (after indent-region activate)
+  "Indent pasted code."
   (if (member major-mode
               '(emacs-lisp-mode lisp-mode c-mode c++-mode latex-mode python-mode))
       (let ((mark-even-if-inactive t))
         (indent-region (region-beginning) (region-end) nil))))
 
-;; When pressing C-k at the end of a line, eat indentation
 (defadvice kill-line (before check-position activate)
+  "When pressing C-k at the end of a line, eat indentation."
   (if (member major-mode '(emacs-lisp-mode scheme-mode lisp-mode
                                            c-mode c++-mode objc-mode
                                            latex-mode plain-tex-mode
@@ -16,9 +21,11 @@
                  (just-one-space 0)
                  (backward-char 1)))))
 
-;; Remove all the carriage returns (^M) from files in dos format
+
 (defun dos2unix ()
-  "Automate M-% C-q C-m RET C-q C-j RET"
+  "Remove all the carriage returns (^M) from files in dos format.
+
+Automate M-% C-q C-m RET C-q C-j RET"
   (interactive)
   (save-excursion
     (goto-char (point-min))
@@ -27,6 +34,7 @@
     (set-buffer-file-coding-system 'unix 't)))
 
 (defun clean-dirty-html ()
+  "Remove font and span tags and align, style and mce_* attributes."
   (interactive)
   (let ((badtags '("font" "span"))
         (badattrs '("align" "style" "mce_[a-z]*")))
@@ -44,40 +52,13 @@
                      (point-min) (point-max))))
 
 (defun html-remove-tables ()
+  "Remove table tags."
   (interactive)
   (perform-replace "</?\\(table\\|tbody\\|tr\\|td\\)[^>]*>" "" 't 't nil nil nil (point-min) (point-max)))
 
-(defun volume-set (volume)
-  "Runs a script that changes to volume. The volume argument can be 0.0 to 1.99 (i think)"
-  (call-process "~/scripts/volume.sh" nil nil nil volume))
-
-(defun volume-set-normal ()
-  "Set the volume to normal"
-  (interactive)
-  (volume-set "0.5")
-  (message "Normal"))
-
-(defun volume-set-loud ()
-  "Set the volume to loud"
-  (interactive)
-  (volume-set "0.8")
-  (message "Loud"))
-
-(defun volume-set-quiet ()
-  "Set the volume to quiet"
-  (interactive)
-  (volume-set "0.3")
-  (message "Quiet"))
-
-(defun volume-set-chopin ()
-  "Chopin is exceptionally quiet"
-  (interactive)
-  (volume-set "1.3")
-  (message "Very loud"))
 
 (defcustom my-kill-emacs-hook '()
-  "Add hooks to this list that have to be called right before
-emacs is killed"
+  "Add hooks to this list that have to be called right before Emacs is killed."
   :type '(repeat function)
   :group 'my-custom-functions)
 
@@ -108,64 +89,61 @@ emacs is killed"
   (message "%d characters" (- (region-end) (region-beginning))))
 
 (defun eval-php (str)
+  "Evaluate a string STR with php."
   (shell-command-to-string
    (concat "php -r " (shell-quote-argument str) "")))
 
 (defun eval-php-region ()
+  "Evaluate PHP code in region."
   (interactive)
   (shell-command-on-region (region-beginning) (region-end) "php -r 'eval(file_get_contents(\"php://stdin\"));'"))
 
+(defun eval-bash-region ()
+  "Evaluate bash commands."
+  (interactive)
+  (shell-command-on-region (region-beginning) (region-end) "bash"))
+
 
 (defun my-php-unserialize-region ()
+  "Unserialize region."
   (interactive)
   (shell-command-on-region
    (region-beginning) (region-end)
    "php -r 'var_export(unserialize(file_get_contents(\"php://stdin\")));'" nil t))
 
 (defun eval-python-region ()
+  "Evaluate python code."
   (interactive)
   (shell-command-on-region (region-beginning) (region-end) "python2"))
 
 (defun generate-password (&optional len)
-  "Generates a string with equal amounts of lower case letters,
-upper case letters and numbers.
- - n   Specify the length of the password. The default is 12."
+  "Generate a string using cli utility 'pwgen'.
+
+Calls 'pwgen' with flags '-y' to include symbols and '-s' to
+generate random (non-memorable) passwords.
+
+ - LEN   Specify the length of the password.  The default is 12."
   (interactive "p")
   (or (and (integerp len) (> len 1)) (setq len 12))
+  (call-process "pwgen" nil t nil "-ys" (number-to-string len) "1"))
 
-  (random t)
-  (let ((result (make-string len ?x))
-        (letters "abcdefghijklmnopqrstuvwxyz")
-        (ltype 0))
-    (dotimes (i len)
-      (setq ltype (random 3))
-      (aset result i
-            (cond ((eq ltype 0)
-                   (aref letters (random (length letters))))
-                  ((eq ltype 1)
-                   (upcase (aref letters (random (length letters)))))
-                  ((eq ltype 2)
-                   (string-to-char (number-to-string (random 9)))))))
-    (insert result)))
+(defun generate-memorable-password (&optional len)
+  "Generate a string that alternates consonants and vowels.
 
-(defun generate-phonetic-password (&optional len)
-  "Generates a string that alternates consonants and vowels to make a
-password that is less secure but more easy to communicate."
+This makes a password that is less secure but more easy to communicate.
+
+Calls 'pwgen' with flags '-B' to exclude ambiguous characters and '-A' to
+exclude capitals.
+
+- LEN	Specify the length of the password.  The default is 12."
   (interactive "p")
   (or (and (integerp len) (> len 1)) (setq len 12))
-
-  (random t)
-  (let ((result (make-string len ?x))
-        (consonants "bcdfghjklmnpqrstvwxyz")
-        (vowels "aeiouy"))
-    (dotimes (i len)
-      (aset result i
-            (if (eq (% i 2) 0)
-                (aref consonants (random (length consonants)))
-              (aref vowels (random (length vowels))))))
-    (insert result)))
+  (call-process "pwgen" nil t nil "-BA" (number-to-string len) "1"))
 
 (defun inotify-message (message &optional title icon)
+  "Create a notification popup using 'notify-send'.
+
+MESSAGE is displayed as the content, prefixed by TITLE and ICON."
   (setq message (shell-quote-argument message))
   (setq title (if title (shell-quote-argument title) ""))
   (if (not icon)
@@ -175,16 +153,18 @@ password that is less secure but more easy to communicate."
                          icon " " title " " message)))
 
 (defun untabify-buffer ()
+  "Run untabify on the buffer."
   (interactive)
   (untabify (point-min) (point-max)))
 
 (defun indent-buffer ()
+  "Indent the buffer."
   (interactive)
   (indent-region (point-min) (point-max)))
 
 (defun cleanup-buffer-safe ()
   "Perform a bunch of safe operations on the whitespace content of a buffer.
-Does not indent buffer, because it is used for a before-save-hook, and that
+Does not indent buffer, because it is used for a 'before-save-hook', and that
 might be bad."
   (interactive)
   (untabify-buffer)
@@ -199,48 +179,36 @@ Including indent-buffer, which should not be called automatically on save."
   (indent-buffer))
 
 (defun my-flymake-show-err ()
-  "Display error message at point"
+  "Display error message at point."
   (interactive)
   (let ((err (get-char-property (point) 'help-echo)))
     (when err
       (message err))))
 
-(defun moz-send-string (str)
-  "The author of mozRepl.el doesn't understand delegates so I had to write this function in order to be
-able to send strings"
-  (comint-send-string (inferior-moz-process)
-                      str))
-(defun moz-stop()
-  (comint-delchar-or-maybe-eof (inferior-moz-process)))
-
-(defun conkeror-reload ()
-  "Reloads the current page in conkeror"
-  (interactive)
-  (moz-send-string "buffers.current.document.location.reload();\n"))
-
-(defun conkeror-open-url (url arg)
-  "Loads the given url conkeror"
-  (moz-send-string (concat "conkeror.browser_object_follow(buffers.current, conkeror.OPEN_NEW_BUFFER, '"
-			   url
-			   "');\n")))
 
 (defun my-get-above-makefile ()
-  (if (not (tramp-tramp-file-p default-directory))
-      (expand-file-name "Makefile"
-                        (let ((d default-directory))
-                          (while (not (or (file-exists-p (expand-file-name
+  "Find the make file."
+  (if (tramp-tramp-file-p default-directory)
+      nil
+    
+    (expand-file-name "Makefile"
+                      (let ((d default-directory))
+                        (while (not (or (file-exists-p (expand-file-name
                                                           "Makefile" d))
                                           (string-equal "/" d)))
-                            (setq d (directory-file-name
-                                     (file-name-directory d))))))
-    nil))
+                            (setq d (file-name-directory
+                                     (directory-file-name d))))
+                        d))))
 
 (defun kill-empty-lines-in-region (start end)
-  "Look through the lines in a region and delete lines that have no
-content or only whitespace."
+  "Look through the lines in a region and delete lines that have no content or
+only whitespace.
+
+START region start.
+END region end."
   (interactive "r")
   (goto-char start)
-  (while (< (point) end) 
+  (while (< (point) end)
     (beginning-of-line)
     (skip-chars-forward " \t")
     (if (eolp)
