45c786bc4a5c3da16482a81e54d56cd2d0389d86
Bram van der Kroef <bram@thebridgeapp.org>
2020-12-24 20:58:08 -0500

    feat: Add restclient and feature-mode

--- load-modes.el
+++ load-modes.el
@@ -386,5 +386,8 @@ In order to have flycheck enabled in web-mode, add an entry to this
 
 (use-package groovy-mode :ensure t)
 
+(use-package restclient :ensure t :mode "\\.rest\\'")
+(use-package feature-mode :ensure t)
+
 (provide 'load-modes)
 ;;; load-modes.el ends here
